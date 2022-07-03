3a132255c6944c9ac5773617134f81a1c7edeb71
Timothy Brits <brits@fastmail.com>
2022-07-03 15:05:27 +0200

    Cleanup

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require('C:\\Users\\timothy.brits\\Documents\\GitHub\\dagga-space\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\Users\\timothy.brits\\Documents\\GitHub\\dagga-space\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\Users\\timothy.brits\\Documents\\GitHub\\dagga-space\\node_modules\\@docusaurus\\theme-classic\\lib\\admonitions.css'),
-  require('C:\\Users\\timothy.brits\\Documents\\GitHub\\dagga-space\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('C:\\Users\\timothy.brits\\Documents\\GitHub\\dagga-space\\src\\css\\custom.css'),
-];
