e2f6577e3a3e5d56c2d40dcb25fe2178fadca3d2
Timothy Brits <brits@fastmail.com>
2022-07-09 14:19:45 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <p className='hero__subtitle'>{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link className='button button--secondary button--lg' to='/strains/intro'>
-            Start exploring 🧭
+            Start Exploring 🧭
           </Link>
         </div>
       </div>
