75dbb60af9eb1a6ec7bf578163cd87262632b7f1
Timothy Brits <brits@fastmail.com>
2022-07-09 14:21:34 +0200

    Emoji changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/duotoneDark');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Dagga Space',
-  tagline: 'Your trip to the cosmos',
+  tagline: 'Your trip to the cosmos 🛸',
   url: 'https://dagga.space/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
