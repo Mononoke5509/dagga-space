4822a666d5a86947da013470d2ace9638733fe3c
Timothy Brits <smart.luck2893@fastmail.com>
2022-07-23 17:32:28 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   url: 'https://dagga.space/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenMarkdownLinks: 'throw',
   favicon: 'img/favicon.ico',
   i18n: {
     defaultLocale: 'en',
@@ -91,7 +91,7 @@ const config = {
             position: 'right',
           },
         ],
-        hideOnScroll: true,
+        hideOnScroll: false,
       },
       footer: {
         style: 'dark',
